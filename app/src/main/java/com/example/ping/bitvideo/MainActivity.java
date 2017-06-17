26df2a39e66cf090ebba78be735388a3126987a6
smtass <deng427@126.com>
2017-06-17 13:09:59 +0800

    save

--- app/src/main/java/com/example/ping/bitvideo/MainActivity.java
+++ app/src/main/java/com/example/ping/bitvideo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ping.bitvideo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gotoMagnet();
+
+    }
+
+    protected void gotoMagnet(){
+        //start magneturlactivity
+        Intent magIntent = new Intent();
+        magIntent.setAction(Intent.ACTION_VIEW);
+        magIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+        //magIntent.setData("magnet:urn:bith:XXXXXXXXXXXXXXXXXXXXXXX");
+        magIntent.setClassName("com.example.ping.bitvideo","com.example.ping.bitvideo.MagnetUrlActivity");
+        startActivity(magIntent);
     }
 }
