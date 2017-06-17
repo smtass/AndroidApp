0fe7a8088e684af486d09ba5fae97e83d4ebb9a0
smtass <deng427@126.com>
2017-06-17 21:41:56 +0800

    V1

--- app/src/main/java/com/example/ping/bitvideo/MainActivity.java
+++ app/src/main/java/com/example/ping/bitvideo/MainActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Intent magIntent = new Intent();
         magIntent.setAction(Intent.ACTION_VIEW);
         magIntent.addCategory(Intent.CATEGORY_BROWSABLE);
-        //magIntent.setData("magnet:urn:bith:XXXXXXXXXXXXXXXXXXXXXXX");
+        magIntent.setData(Uri.parse("magnet:?xt=urn:btih:03621694F0E8B2CE87216C99CB5CA3AF23029E37"));
         magIntent.setClassName("com.example.ping.bitvideo","com.example.ping.bitvideo.MagnetUrlActivity");
         startActivity(magIntent);
     }
